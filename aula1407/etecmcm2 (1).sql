2ded4f872aec5587c328260a2b6ba467de3fd287
Mayara de Oliveira Sant'Anna <159502755+mayarasantanna2@users.noreply.github.com>
2025-05-14 12:46:02 -0300

    Add files via upload

---
+++ aula1407/etecmcm2 (1).sql
@@ -0,0 +1,152 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 14-Maio-2025 às 17:45
+-- Versão do servidor: 10.4.22-MariaDB
+-- versão do PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `etecmcm2`
+--
+CREATE DATABASE IF NOT EXISTS `etecmcm2` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `etecmcm2`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `nome_cat` varchar(255) DEFAULT NULL,
+  `id_categoria` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `categoria`
+--
+
+INSERT INTO `categoria` (`nome_cat`, `id_categoria`) VALUES
+('Limpeza', 1),
+('Açogue', 2),
+('Hortifruti', 3),
+('Enlatados', 4),
+('Higiene', 5),
+('Laticinios', 6),
+('Doces', 7),
+('Eletronicos', 8),
+('Brinquedos', 9),
+('Bebidas', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id_categoria` int(11) NOT NULL,
+  `nome_cat` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `categorias`
+--
+
+INSERT INTO `categorias` (`id_categoria`, `nome_cat`) VALUES
+(1, 'laticinios'),
+(2, 'higiene'),
+(3, 'limpeza'),
+(4, 'hortifruti'),
+(5, 'enlatados'),
+(7, 'açougue');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `produtos`
+--
+
+CREATE TABLE `produtos` (
+  `id_prod` int(11) NOT NULL,
+  `nome` varchar(255) DEFAULT NULL,
+  `preco` decimal(10,2) DEFAULT NULL,
+  `marca` varchar(255) DEFAULT NULL,
+  `qtde` int(11) DEFAULT NULL,
+  `fk_categoria_id_categoria` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Extraindo dados da tabela `produtos`
+--
+
+INSERT INTO `produtos` (`id_prod`, `nome`, `preco`, `marca`, `qtde`, `fk_categoria_id_categoria`) VALUES
+(1, 'Detergente', '3.50', 'limpol', 50, NULL),
+(2, 'Detergente', '3.50', 'limpol', 50, 1),
+(3, 'sabao em po', '13.50', 'omo', 50, 1),
+(4, 'Picanha', '100.50', 'Freboi', 50, 2),
+(5, 'Peito de frango', '100.50', 'Seara', 50, 2),
+(6, 'Banana', '13.50', 'Agranor', 50, 3),
+(7, 'Maça', '10.50', 'Arcanjo', 50, 3),
+(8, 'Milho', '5.50', 'quero', 50, 4),
+(9, 'Ervilha', '6.50', 'knorr', 50, 4);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices para tabela `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Índices para tabela `produtos`
+--
+ALTER TABLE `produtos`
+  ADD PRIMARY KEY (`id_prod`),
+  ADD KEY `FK_produtos_2` (`fk_categoria_id_categoria`);
+
+--
+-- AUTO_INCREMENT de tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT de tabela `produtos`
+--
+ALTER TABLE `produtos`
+  MODIFY `id_prod` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Restrições para despejos de tabelas
+--
+
+--
+-- Limitadores para a tabela `produtos`
+--
+ALTER TABLE `produtos`
+  ADD CONSTRAINT `FK_produtos_2` FOREIGN KEY (`fk_categoria_id_categoria`) REFERENCES `categoria` (`id_categoria`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
